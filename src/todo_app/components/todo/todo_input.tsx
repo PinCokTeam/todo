915829adf19886ccccd19e6c7e6103078762b832
MINT <pb1123love@gmail.com>
2024-03-14 23:31:33 +0900

    ✨ ui folder refactoring

--- src/todo_app/components/todo/todo_input.tsx
+++ src/todo_app/components/todo/todo_input.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { useTodo } from "@/lib/providers/todo";
-import { CircleIcon } from "../icons";
-import { Input } from "../input";
+import { CircleIcon } from "../ui/icons";
+import { Input } from "../ui/input";
+import { useState } from "react";
 
 export default function ToDoInput() {
   const { refreshTodoList } = useTodo();
 
+  const [inputValue, setInputValue] = useState("");
+
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
+      setInputValue("");
       fetch("api/todo", {
         method: "POST",
         body: JSON.stringify({ title: e.currentTarget.value }),
@@ -24,6 +28,10 @@ export default function ToDoInput() {
     }
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="flex items-center rounded-md border-2 border-gray-200 p-2">
       <CircleIcon className="h-5 w-5 text-gray-400" />
@@ -32,6 +40,8 @@ export default function ToDoInput() {
         placeholder="Create a new todo..."
         type="text"
         onKeyDown={handleKeyDown}
+        onChange={handleChange}
+        value={inputValue}
       />
     </div>
   );
--- src/todo_app/components/todo/todo_list.tsx
+++ src/todo_app/components/todo/todo_list.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { CircleIcon } from "../icons";
 import { useTodo } from "@/lib/providers/todo";
+import ToDo from "./todo";
 
 export default function ToDoList() {
   const { todoList, refreshTodoList } = useTodo();
 
   useEffect(() => {
     refreshTodoList();
-  });
+  }, [refreshTodoList]);
 
   return (
     <div className="flex flex-col items-center">
@@ -17,10 +17,12 @@ export default function ToDoList() {
         <p className="text-gray-400">You have no todo items.</p>
       ) : (
         todoList.map((todo) => (
-          <div className="flex items-center w-full p-2 gap-4" key={todo.id}>
-            <CircleIcon className="h-5 w-5 text-gray-400 hover:text-green-500 cursor-pointer" />
-            <p className="text-gray-800 dark:text-white">{todo.title}</p>
-          </div>
+          <ToDo
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            choice={todo.choice}
+          />
         ))
       )}
     </div>
