b7087e732839b8c064639b6029aa639f207ec445
kimhanseop <fghjk000@naver.com>
2024-03-27 14:27:55 +0900

    todo-ViewSet

--- src/todo_core/to_do/serializer.py
+++ src/todo_core/to_do/serializer.py
@@ -3,7 +3,7 @@ from rest_framework.serializers import ModelSerializer
 from to_do.models import ToDo
 
 
-class ToDoListSerializer(ModelSerializer):
+class ToDoSerializer(ModelSerializer):
     class Meta:
         model = ToDo
         fields = ['id', 'title', 'choice']
--- src/todo_core/to_do/views.py
+++ src/todo_core/to_do/views.py
@@ -1,40 +1,21 @@
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from to_do.models import ToDo
-from to_do.serializer import ToDoListSerializer
+from to_do.serializer import ToDoSerializer
 
 
-class ToDoListCreateAPI(APIView):
-    def get(self, request):
-        todo = ToDo.objects.all()
-        return_data = ToDoListSerializer(todo, many=True).data
-        return Response(return_data)
-
-    def post(self, request):
-        serializer = ToDoListSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        todo = ToDo.objects.create(
-            title=serializer.validated_data['title'],
-        )
-
-        return_data = ToDoListSerializer(todo).data
-        return Response(return_data, status=201)
+class ToDoViewSet(ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
 
 
-class ToDoDetailAPI(APIView):
-    def get(self, request, todo_id):
-        todo = get_object_or_404(ToDo, id=todo_id)
-        return_data = ToDoListSerializer(todo).data
-        return Response(return_data, status=200)
+todolist = ToDoViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
 
-    def patch(self, request, todo_id):
-        todo = get_object_or_404(ToDo, id=todo_id)
-        serializer = ToDoListSerializer(instance=todo, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(data=serializer.data)
-
-    def delete(self, request, todo_id):
-        get_object_or_404(ToDo, id=todo_id).delete()
-        return Response(status=204)
+todo_detail = ToDoViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
