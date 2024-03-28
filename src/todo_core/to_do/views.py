0905d53c63b7c6bd5b7578027b41964129a883c9
김민규 <minkyu.kim@birdview.kr>
2024-03-28 17:01:27 +0900

    ✨ Add token authentication to LoginAPIView and ToDoListCreateAPI.

--- src/todo_core/to_do/views.py
+++ src/todo_core/to_do/views.py
@@ -5,8 +5,37 @@ from to_do.models import ToDo
 from to_do.serializer import ToDoListSerializer
 
 
+class LoginAPIView(APIView):
+    def post(self, request):
+        user_id = request.data['id']
+        password = request.data['password']
+
+        if not User.objects.filter(user_id=user_id).exists():
+            return Response(status=401)
+
+        db_user_data = User.objects.get(user_id=user_id)
+        if db_user_data.password != password:
+            return Response(status=401)
+
+        token = Token.objects.create(token=random.random())
+
+        return Response(data={'token': token})
+
+class LogoutAPIView(APIView):
+    def delete(self, request):
+        token = request.query_params['token']
+        Token.objects.filter(token=token).delete()
+        return Response()
+
 class ToDoListCreateAPI(APIView):
     def get(self, request):
+        # token 검사로직 추가, 만약 통과하면 아래 로직 그대로 실행
+        if 'token' not in request.query_params:
+            return Response(status=401)
+        token = request.query_params['token']
+        if not Token.objects.filter(token=token).exists():
+            return Response(status=401)
+
         todo = ToDo.objects.all()
         return_data = ToDoListSerializer(todo, many=True).data
         return Response(return_data)
